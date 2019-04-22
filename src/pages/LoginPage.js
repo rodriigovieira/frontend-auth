0044d88fb3ee09415ba5ab1e743c521e686d5f55
Rodrigo Vieira <rodriigovieira@gmail.com>
2019-04-22 18:14:27 -0300

    ➕ Adding login logic

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -13,63 +13,112 @@ import {
 } from "mdbreact";
 import { Link } from 'react-router-dom';
 
+import axios from 'axios'
+
 import './LoginPage.css';
 
-const LoginPage = () => {
-  return (
-    <MDBContainer>
-      <MDBRow className="form">
-        <MDBCol md="5">
-          <MDBCard>
-            <MDBCardBody>
-              <MDBCardHeader className="form-header deep-blue-gradient rounded">
-                <h3 className="my-3">
-                  <MDBIcon icon="lock" /> Login
+export default class LoginPage extends React.Component {
+  state = {
+    email: "",
+    password: "",
+    error: false
+  }
+
+  handleLogin = (e) => {
+    e.preventDefault()
+
+    axios.post('http://localhost:3000/api/users/login', {
+      email: this.state.email,
+      password: this.state.password
+    })
+      .then(res => {
+        if (res.status !== 200) {
+          this.setState({ error: true })
+          return null
+        }
+
+        this.setState({ error: false })
+
+        console.log(res)
+
+        localStorage.setItem('csrf', res.data.csrfToken)
+        localStorage.setItem('token', res.data.token)
+
+        this.props.history.push('/usuarios')
+      })
+      .catch(e => {
+        this.setState({ error: true })
+        console.log(e)
+      })
+  }
+
+  handleChange = (e) => {
+    const { name, value } = e.target
+
+    this.setState({ [name]: value })
+  }
+
+  render() {
+    return (
+      <MDBContainer>
+        <MDBRow className="form">
+          <MDBCol md="4">
+            <MDBCard>
+              <MDBCardBody>
+                <MDBCardHeader className="form-header deep-blue-gradient rounded">
+                  <h3 className="my-3">
+                    <MDBIcon icon="lock" /> Login
                 </h3>
-              </MDBCardHeader>
-              <form>
-                <div className="grey-text">
-                  <MDBInput
-                    label="Digite seu e-mail"
-                    icon="envelope"
-                    group
-                    type="email"
-                    validate
-                    error="wrong"
-                    success="right"
-                  />
-                  <MDBInput
-                    label="Digite sua senha"
-                    icon="lock"
-                    group
-                    type="password"
-                    validate
-                  />
-                </div>
+                </MDBCardHeader>
 
-                <div className="text-center mt-4">
-                  <MDBBtn
-                    color="light-blue"
-                    className="mb-3"
-                    type="submit"
-                  >
-                    <Link to="/usuarios">
-                      Efetuar Login
-                  </Link>
-                  </MDBBtn>
-                </div>
-              </form>
-              <MDBModalFooter>
-                <div className="font-weight-light">
-                  <Link to="/registrar">Criar Conta</Link>
-                </div>
-              </MDBModalFooter>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-      </MDBRow>
-    </MDBContainer>
-  );
-};
+                {this.state.error && ('Login ou senha incorretos.')}
+                
+                <form>
+                  <div className="grey-text">
+                    <MDBInput
+                      label="Digite seu e-mail"
+                      icon="envelope"
+                      group
+                      type="email"
+                      validate
+                      error="wrong"
+                      success="right"
+                      name="email"
+                      onChange={this.handleChange}
+                    />
+                    <MDBInput
+                      label="Digite sua senha"
+                      icon="lock"
+                      group
+                      type="password"
+                      validate
+                      name="password"
+                      onChange={this.handleChange}
+                    />
+                  </div>
 
-export default LoginPage;
+                  <div className="text-center mt-4">
+                    <MDBBtn
+                      color="light-blue"
+                      className="mb-3"
+                      type="submit"
+                      onClick={this.handleLogin}
+                    >
+
+                        Efetuar Login
+                    </MDBBtn>
+                  </div>
+                </form>
+                <MDBModalFooter>
+                  <div className="font-weight-light">
+                    <Link to="/registrar">Criar Conta</Link>
+                  </div>
+                </MDBModalFooter>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+    )
+  }
+}

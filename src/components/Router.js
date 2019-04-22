12eb461bb620b3477ba6ff7fc7850e9de663658b
Rodrigo Vieira <rodriigovieira@gmail.com>
2019-04-22 15:22:58 -0300

    ✏️ Adding Edit User Route

--- src/components/Router.js
+++ src/components/Router.js
@@ -5,6 +5,7 @@ import LoginPage from '../pages/LoginPage'
 import SignUpPage from '../pages/SignUpPage'
 import UsersPage from '../pages/UsersPage'
 import CreateUserPage from '../pages/CreateUserPage'
+import EditUserPage from '../pages/EditUserPage'
 
 export default class Router extends Component {
   render() {
@@ -15,6 +16,7 @@ export default class Router extends Component {
           <Route path="/registrar" component={SignUpPage} />
           <Route path="/usuarios" component={UsersPage} />
           <Route path="/criar" component={CreateUserPage} />
+          <Route path="/editar" component={EditUserPage} />
         </Switch>
       </BrowserRouter>
     )
